3f4f7fe56460091788ec38eb838d1645cc077739
Fatesaid <fatesaid56@gmail.com>
2024-07-24 18:14:18 +0200

    add buildTree

--- bst.js
+++ bst.js
@@ -1,3 +1,27 @@
-function Tree(array) {
-  function buildTree(array) {}
+import { mergeSort } from "./mergeSort.js";
+import { Node } from "./node.js";
+import { prettyPrint } from "./prettyPrint.js";
+
+function buildTree(array) {
+  if (array.length === 0) {
+    return null;
+  } else {
+    let middle = Math.floor(array.length / 2);
+    let node = Node(array[middle]);
+    node.left = buildTree(array.slice(0, middle));
+    node.right = buildTree(array.slice(middle + 1, array.length));
+
+    return node;
+  }
 }
+
+function Tree(array) {
+  let sortedArray = mergeSort(array);
+  let root = buildTree(sortedArray);
+
+  return { root };
+}
+
+let a = Tree([1, 2, 3, 4, 5, 6, 7]);
+
+prettyPrint(a.root);
